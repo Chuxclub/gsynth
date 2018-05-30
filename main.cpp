015f33105854224f1633516fd5f4b370f3cf7572
Florian <florian-1992@hotmail.fr>
2018-05-30 17:50:00 +0200

    Reworking skeleton to improve readibility

--- main.cpp
+++ main.cpp
@@ -36,68 +36,144 @@ int main()
 
         else if (choice == "gHelp")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "COMMAND: git help <command> OR  man git-<command>" << "\n";
+            std::cout << "USE: Summon exhaustive help on how to use <command>" << "\n";
+            std::cout << "OPTIONS: -h => give more concise help. Example: git add -h" << "\n\n";
         }
 
         else if (choice == "gConfig")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "~~~~~~~~~~~~ COMMAND ~~~~~~~~~~~~ " << "\n\n";
+
+            std::cout << "git config" << "\n";
+            std::cout << "USE: Reads and writes contributor's git settings";
+            std::cout << "\n\n\n";
+
+
+            std::cout << " ~~~~~~~~~~~~ OPTION(S) ~~~~~~~~~~~~ " << "\n\n";
+
+            std::cout << "NONE + argument" << "\n";
+            std::cout << "USE: Reads and writes values in tracked project only" << "\n\n";
+
+            std::cout << "--global" << "\n";
+            std::cout << "USE: Reads and writes values specific personally to you, the user." << "\n\n";
+
+            std::cout << "--system" << "\n";
+            std::cout << "USE: Reads and writes values applied to every user on the system and all their repositories." << "\n\n";
+
+            std::cout << "--list" << "\n";
+            std::cout << "USE: List all Git settings (You can also check a specific key’s value is by typing git config <key>)";
+            std::cout << "\n\n\n";
+
+
+            std::cout << "~~~~~~~~~~~~ ARGUMENT(S) ~~~~~~~~~~~~ " << "\n\n";
+
+            std::cout << "alias.<shortcut> <command>" << "\n\n";
+            std::cout << "USE: Creates shortcuts (names defined by user) of git commands and stores them in <options> settings." << "\n";
+            std::cout << "Start the command with a '!' to refer to an external command (not a git subcommand as with gitk)." << "\n";
+            std::cout << "Useful shortcuts to implement: alias.co checkout // alias.br branch // alias.ci commit // alias.st status" << "\n";
+            std::cout << "alias.unstage 'reset HEAD --' // alias.last 'log -1 HEAD'" << "\n\n";
+
+            std::cout << "core.editor emacs" << "\n";
+            std::cout << "USE: Changes default text editor that will be used when Git needs you to type in a message" << "\n";
+
+            std::cout << "user.email johndoe@example.com " << "\n";
+            std::cout << "USE: Sets contributor's email" << "\n";
+
+            std::cout << "user.name \"John Doe\" " << "\n";
+            std::cout << "USE: Sets contributor's name ";
+            std::cout << "\n\n\n";
         }
 
         else if (choice == "gTrack")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
+
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
+
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
+
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
+
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
+
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
+
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
+
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
         }
 
         else if (choice == "gRemote_repo")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
         }
 
         else if (choice == "gBranch")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
         }
 
         else if (choice == "gRemote_branch")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
         }
 
         else if (choice == "gLog")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
         }
 
         else if (choice == "gTag")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
         }
 
         else if (choice == "gRemote_tag")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
         }
 
         else if (choice == "gAmend")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
         }
 
         else if (choice == "gUntrack")
         {
-            std::cout << "HOW TO SUMMON: " << "\n\n";
-            std::cout << "USE: " << "\n\n";
+            std::cout << "COMMAND: " << "\n";
+            std::cout << "USE: " << "\n";
+            std::cout << "OPTIONS: " << "\n\n";
         }
 
         else
